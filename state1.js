ace27f76c4151802079ea43867c35fe25bcb14f6
Patrick Martin <patrickmartin@Patricks-MacBook-Pro.local>
2021-01-30 18:44:28 -0600

    Add changeState event listener to each state

--- state1.js
+++ state1.js
@@ -2,6 +2,35 @@ var demo = {};
 demo.state1 = function(){};
 demo.state1.prototype = {
     preload: function(){},
-    create: function(){},
+    create: function(){
+        game.stage.backgroundColor = '#DDDDDD';
+        console.log('state1');
+       /* game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(changeState, null, null, 1);
+         game.input.keyboard.addKey(Phaser.Keyboard.TWO).onDown.add(changeState, null, null, 2);
+        */
+        //Or instead can do this with function listed below
+        addChangeStateEventListeners();
+    },
     update: function(){}
 };
+
+function changeState(i,stateNum){
+    console.log(i);
+    game.state.start('state' + stateNum);
+}
+
+function addKeyCallback(key, fn, args){
+game.input.keyboard.addKey(key).onDown.add(fn, null, null, args);
+}
+
+function addChangeStateEventListeners(){
+    addKeyCallback(Phaser.Keyboard.ONE, changeState,1);
+    addKeyCallback(Phaser.Keyboard.TWO, changeState,2);
+    addKeyCallback(Phaser.Keyboard.THREE, changeState,3);
+    addKeyCallback(Phaser.Keyboard.FOUR, changeState,4);
+    addKeyCallback(Phaser.Keyboard.FIVE, changeState,5);
+    addKeyCallback(Phaser.Keyboard.SIX, changeState,6);
+    addKeyCallback(Phaser.Keyboard.SEVEN, changeState,7);
+    addKeyCallback(Phaser.Keyboard.EIGHT, changeState,8);
+    addKeyCallback(Phaser.Keyboard.NINE, changeState,9);
+}
